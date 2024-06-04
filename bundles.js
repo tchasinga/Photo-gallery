9815c105fb51fceb26d16ce64573b8f614c25096
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-04 13:00:48 +0200

    Refactor CSS class names for consistency and clarity

--- bundles.js
+++ bundles.js
@@ -19,15 +19,15 @@ mainProjectsContainer.innerHTML += elementObject.map((element) => {
     return `
     <div class="card">
       <div class="card-container">
-        <div class="card-image">
+        <div class="cardimage">
           <img src="${element.image}" alt="image" />
         </div>
         <div class="card-content">
-          <div class="card-title">
+          <div class="cardtitle">
           <h1>${element.name}</h1>
           <p>${element.country}</p>
         </div>
-        <div class="card-action">
+        <div class="cardaction">
           <p class="btn">${element.more}</p>
           <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="aqua" stroke-width="2" d="M2,12 L22,12 M13,3 L22,12 L13,21"></path></svg>
           </div>
@@ -38,4 +38,14 @@ mainProjectsContainer.innerHTML += elementObject.map((element) => {
 }).join('');
 
 // Initialising the modal and adding all needed animation
+
+const Cardimage = document.querySelector('.cardimage');
+const CardTitle = document.querySelector('.cardtitle');
+const cardActions = document.querySelectorAll('.cardaction');
+
+cardActions.forEach(cardAction => {
+    cardAction.style.display = 'none';
+});
+
+
 
