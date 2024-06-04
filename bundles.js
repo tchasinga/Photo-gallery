e2a0ab9bc9aeea02ccc2763725c902b1f21b78a8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-04 16:24:36 +0200

    checking file

--- bundles.js
+++ bundles.js
@@ -26,15 +26,18 @@ mainProjectsContainer.innerHTML += elementObject
       <h1>${element.name}</h1>
       <p>${element.country}</p>
     </div>
-       <div class="cardaction">
-       <p class="btn">${element.more}</p>
-       <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-         <path fill="none" stroke="aqua" stroke-width="2" d="M2,12 L22,12 M13,3 L22,12 L13,21"></path>
-       </svg>
-       </div>
-      </div>
-      </div>
+    <div class="cardaction">
+    <p class="btn">${element.more}</p>
+    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+      <path fill="none" stroke="aqua" stroke-width="2" d="M2,12 L22,12 M13,3 L22,12 L13,21"></path>
+    </svg>
     </div>
+      </div>
+      
+      </div>
+      
+    </div>
+    
   </div>
     `;
   })
