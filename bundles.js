238eec298af4892a470c5c9d781f3da1cf4f534c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-04 10:45:31 +0200

    chore: Update CSS and HTML files, add mainroot constant in bundles.js

--- bundles.js
+++ bundles.js
@@ -18,6 +18,16 @@ const mainProjectsContainer = document.querySelector('.mainroot');
 mainProjectsContainer.innerHTML += elementObject.map((element) => {
     return `
     <div class="card">
+      <div class="card-container">
+        <div class="card-image">
+          <img src="${element.image}" alt="image" />
+        </div>
+        <div class="card-content">
+          <h2>${element.name}</h2>
+          <p>${element.country}</p>
+          <button class="btn">${element.more}</button>
+        </div>
+      </div>
     </div>
     `;
 }).join('');
