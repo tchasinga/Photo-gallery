a801ddb2e0cbb91ac06fb24300d6043e2a55c405
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-04 10:32:05 +0200

    chore: Update CSS and HTML files, remove unused code

--- bundles.js
+++ bundles.js
@@ -1,11 +1,24 @@
 import elementObject from './Tools/data.js';
 
-const mainProjectsContainer = document.querySelector('.mainroot');
+
+// Auto changing title
+
+let AlertShow = false;
+
+setInterval(() => {
+  document.title = AlertShow
+    ? 'Photo Gallery'
+    : 'Welcome to the Photo Gallery';
+
+  AlertShow = !AlertShow;
+}, 3000);
 
 // Displaying the elements on the screen
+const mainProjectsContainer = document.querySelector('.mainroot');
 mainProjectsContainer.innerHTML += elementObject.map((element) => {
     return `
     <div class="card">
+       
         <div class="card-image">
             <img src="${element.image}" alt="image">
         </div>
@@ -13,6 +26,7 @@ mainProjectsContainer.innerHTML += elementObject.map((element) => {
             <h2>${element.name}</h2>
             <p>${element.country}</p>
             <button>${element.more}</button>
+        </div>
         </div>
     </div>
     `;
