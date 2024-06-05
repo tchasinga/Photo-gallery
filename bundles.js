9a47716d22def1bb3a5a5bb0a8ae7af903d9de66
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-05 08:27:41 +0200

    chore: Refactor card layout in bundles.js

--- bundles.js
+++ bundles.js
@@ -17,25 +17,25 @@ mainProjectsContainer.innerHTML += elementObject
     return `
     <div class="card">
     <div class="card-container">
-      <div class="cardimage">
-        <img src="${element.image}" alt="image" />
-      </div>
-         <div class="card-content">
-        <div class="modalPostion">
-      <div class="cardtitle">
-      <h1>${element.name}</h1>
-      <p>${element.country}</p>
+        <div class="cardimage">
+            <img src="${element.image}" alt="image" />
+        </div>
+        <div class="card-content">
+            <div class="modalPostion">
+                <div class="cardtitle">
+                    <h1>${element.name}</h1>
+                    <p>${element.country}</p>
+                </div>
+                <div class="cardaction">
+                    <p class="btn">${element.more}</p>
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <path fill="none" stroke="aqua" stroke-width="2" d="M2,12 L22,12 M13,3 L22,12 L13,21"></path>
+                    </svg>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="cardaction">
-    <p class="btn">${element.more}</p>
-    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-      <path fill="none" stroke="aqua" stroke-width="2" d="M2,12 L22,12 M13,3 L22,12 L13,21"></path>
-    </svg>
-    </div>
-    </div>
-     </div>
-    </div>
-  </div>
+</div>
     `;
   })
   .join("");
